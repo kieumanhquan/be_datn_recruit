6e2649faa07f1159b23e39beb9f09273bd73a86f
hoanggiap1234 <hoanggiap6984@gmail.com>
2022-03-21 09:27:41 +0700

    fix base

--- src/main/java/com/itsol/recruit/security/UserDetailServiceImpl.java
+++ src/main/java/com/itsol/recruit/security/UserDetailServiceImpl.java
@@ -1,12 +1,12 @@
 package com.itsol.recruit.security;
 
 
+import com.itsol.recruit.entity.Role;
 import com.itsol.recruit.entity.User;
 import com.itsol.recruit.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,7 +17,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -31,24 +33,26 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        try{
+
             User user = userRepositoryJpa.findByUserName(username);
-            if (user !=null ){
-                return  createSpringSecurityUser(username, user);
-            } else {
-                return null;
+            if(user== null){
+                throw  new UsernameNotFoundException("usesername not found");
             }
-        }catch (Exception e){
-            log.error("username is not exist!");
-            return  null;
-        }
+            return createSpringSecurityUser(username, user);
+
     }
 
     private org.springframework.security.core.userdetails.User createSpringSecurityUser(String userName, User user) {
         if (!user.isActive()) {
             throw new UserNotActivatedException("User " + userName + " was not activated");
         }
-        List<GrantedAuthority> grantedAuthorities = user.getRoles().stream().map(authority -> new SimpleGrantedAuthority(authority.getRoleCode())).collect(Collectors.toList());
+        // List<GrantedAuthority> grantedAuthorities = user.getRoles().stream().map(authority -> new SimpleGrantedAuthority(authority.getRoleCode())).collect(Collectors.toList());
+        // có thể thay thế bằng đoạn code bên dưới cho dễ hiểu
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Set<Role> roles = user.getRoles();
+        for (Role role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRoleCode()));
+        }
         return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), grantedAuthorities);
     }
 

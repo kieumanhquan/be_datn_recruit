e2cd83fb838c7a7bb1e65a2863e72e7bee6219d8
Hieu <Thanglong@gmail.com>
2022-04-01 01:39:08 +0700

    add Sort

--- src/main/java/com/itsol/recruit/service/impl/UserServiceImpl.java
+++ src/main/java/com/itsol/recruit/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.itsol.recruit.service.impl;
 
+import com.itsol.recruit.entity.Role;
 import com.itsol.recruit.entity.User;
+import com.itsol.recruit.repository.RoleRepository;
 import com.itsol.recruit.repository.UserRepository;
 import com.itsol.recruit.service.UserService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,8 +18,11 @@ public class UserServiceImpl implements UserService {
 
     public final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public final RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -37,6 +43,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElse(null);
+    }
+
+    @Override
+    public List<User> findUserByRole(String role) {
+       List<User> users = userRepository.findAll();
+       List<User> userList = new ArrayList<>();
+       for(User u: users){
+           for (Role role1: u.getRoles()){
+               if (role1.getCode().equals(role)){
+                   userList.add(u);
+                   break;
+               }
+           }
+       }
+        return userList;
     }
 
 
--- src/main/java/com/itsol/recruit/web/UserController.java
+++ src/main/java/com/itsol/recruit/web/UserController.java
@@ -4,10 +4,7 @@ import com.itsol.recruit.core.Constants;
 import com.itsol.recruit.entity.User;
 import com.itsol.recruit.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +24,18 @@ public class UserController {
         return  ResponseEntity.ok().body( userService.getAllUser());
     }
 
-    @GetMapping(value = "/user/{id}")
-    public ResponseEntity<User> findUserById(@RequestParam("id") Long id){
+    @GetMapping(value = "/user/id={id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
         return  ResponseEntity.ok().body( userService.findById(id));
+    }
+
+    @GetMapping(value = "/user/username={username}")
+    public ResponseEntity<User> findUserByUserName(@PathVariable String username){
+        return  ResponseEntity.ok().body( userService.findUserByUserName(username));
+    }
+
+    @GetMapping(value = "/user/role={role}")
+    public ResponseEntity<List<User>> findUserByURole(@PathVariable String role){
+        return  ResponseEntity.ok().body( userService.findUserByRole(role));
     }
 }

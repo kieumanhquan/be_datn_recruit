4857040d9f1c99ec1f770a1836584c6e9b44054c
Hieu <Thanglong@gmail.com>
2022-04-04 13:57:57 +0700

    update

--- src/main/java/com/itsol/recruit/repository/UserRepository.java
+++ src/main/java/com/itsol/recruit/repository/UserRepository.java
@@ -1,16 +1,11 @@
 package com.itsol.recruit.repository;
 
-import com.itsol.recruit.entity.Role;
 import com.itsol.recruit.entity.User;
 import com.itsol.recruit.repository.repoext.UserRepositoryExt;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryExt {
